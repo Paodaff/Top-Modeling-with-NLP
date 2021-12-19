95ee85f2bf3e6c0920f9af6230d310324c626845
Alina-sul <48648714+Alina-sul@users.noreply.github.com>
2021-12-20 00:27:06 +0200

    updated mongo - cleaned html - has error

--- Connect to elasticsearch/elastic_connect.py
+++ Connect to elasticsearch/elastic_connect.py
@@ -1,37 +1,59 @@
-
 # import requests module
 import json
 import pymongo
 import requests
 from requests.auth import HTTPBasicAuth
+from utils import *
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["NLP_project"]
 mycol = mydb["elasticsearch"]
 
+
 def get_json(url: str, user: str, password: str):
     # Making a get request
     response = requests.get(f'{url}',
-                auth = HTTPBasicAuth(f'{user}', f'{password}'))
-    
+                            auth=HTTPBasicAuth(f'{user}', f'{password}'))
+
     return json.dumps(response.json(), indent=4, sort_keys=True, ensure_ascii=False)
 
+
 json_answer = get_json(
-    "https://es1.mediaquantum.eu/streamua/streamua/_search?pretty=true&q=domain:*.il",
+    "https://es1.mediaquantum.eu/streamua/streamua/_search?pretty=true&q=domain:*.il&sort=timestamp%3Adesc&size=2000",
     "elastic",
     "changeme")
 
 json_answer = json.loads(json_answer)
-json_answer = json_answer['hits']['hits'] # Ask slava what does max_score and total stand for in hits 
+json_answer = json_answer['hits']['hits']
 
+count = 0
 
-for document in json_answer: # Document keys : ['_id', '_index', '_score', '_source', '_type']
+for document in json_answer:  # Document keys : ['_id', '_index', '_score', '_source', '_type']
     # Id and source are the only keys that make sense.
-    mycol.insert_one({
-        "id": document['_id'],
-        "title": document['_source']['title'],
-        "url": document['_source']['url'],
-        "date": document['_source']['insertdate'],
-        "text": document['_source']['text']})
-    
 
+    # declarations
+    html = document['_source']['text']
+    irrelevantSitesNames = ["yaaraflowers", "bum", "tusa",
+                            "love", "date", "b-zoog", "lov",
+                            "makirim", "click4", "email",
+                            "atraf", "soulmate", "soul"]
+
+    # Relevancy check
+    checkURL = [x for x in irrelevantSitesNames if (x in document['_source']['url'])]
+    checkTitle = (document['_source']['title'] != "")
+
+    # creation of needed elements
+    abstract = find_abstract(html)
+    body = find_body(html)
+    summary = find_summary(html)
+
+    if not checkURL and checkTitle and abstract and summary:
+        mycol.insert_one({
+                    "id": document['_id'],
+                    "title": document['_source']['title'],
+                    "url": document['_source']['url'],
+                    "date": document['_source']['date'],
+                    "abstract": abstract,
+                    "body": body,
+                    "summary": summary
+        })
---
+++ Connect to elasticsearch/utils.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+
+
+def find_abstract(html):
+    soup = BeautifulSoup(html, "html.parser")
+    abstract = soup.find("h1") or soup.find("h2")
+    if abstract:
+        return abstract.text
+    return None
+
+
+def find_body(html):
+    soup = BeautifulSoup(html, features="html.parser")
+    body = soup.find_all("div") or soup.find_all("p")
+    if body:
+        return body
+    return None
+
+
+def find_summary(html):
+    soup = BeautifulSoup(html, features="html.parser")
+    body = soup.find_all("div") or soup.find_all("p")
+    if body:
+        return body[-1].get_text()
+    return None
